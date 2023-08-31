a19ab057b9074f2a979d82333f4426121a042fb8
Klaus <101809208+LucasRehmann@users.noreply.github.com>
2023-08-31 15:40:59 -0300

    API Almost finished

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ import matplotlib.pyplot as plt
 import cv2
 from numpy.lib.polynomial import poly
 from PIL import Image, ImageDraw
+from fastapi import Request
 #from sklearn.cluster import KMeans
 import urllib.request
 
 app = FastAPI()
 
 @app.post("/process_image/")
-async def process_image(image_url: str):
+async def process_image(request: Request):
     try:
+        data = await request.json()
+        image_url = data.get("image_url")
         req = urllib.request.urlopen(image_url)
         arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
         img = cv2.imdecode(arr, -1) # 'Load it as it is'
 
         #cv2_imshow(img)
         if cv2.waitKey() & 0xff == 27: quit()
+
+        if img.shape[::2] != (400,400):
+            crop = cv2.resize(img, (int(int(img.shape[1]) * 400/int(img.shape[1])), int(int(img.shape[0]) * 400/int(img.shape[0]))), cv2.INTER_AREA)
 
         if img.shape[::2] != (400,400):
             crop = cv2.resize(img, (int(int(img.shape[1]) * 400/int(img.shape[1])), int(int(img.shape[0]) * 400/int(img.shape[0]))), cv2.INTER_AREA)
@@ -45,17 +51,16 @@ async def process_image(image_url: str):
         final_img_arr = cv2.subtract(dilated,x)
         deganger = (Image.fromarray(final_img_arr))
 
-        """
-        final_img_arr = np.dstack((img_arr, lum_img_arr))
-        deganger = (Image.fromarray(final_img_arr))
-        open_cv_image = np.array(deganger)
-        # Convert RGB to BGR
-        open_cv_image = open_cv_image[:, :, ::-1].copy()
-        """
-        x = cv2.subtract(img_arr, lum_img_arr)
+        arrImg = np.array(deganger)
+        cvImg = cv2.cvtColor(arrImg, cv2.COLOR_RGB2BGR)
+        escalope = cv2.cvtColor(cvImg, cv2.COLOR_BGR2GRAY)
 
-        final_img_arr = cv2.subtract(dilated,x)
-        deganger = (Image.fromarray(final_img_arr))
+        (cnt, hierarchy) = cv2.findContours(escalope, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        rgb = cv2.cvtColor(escalope, cv2.COLOR_BGR2RGB)
+        cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
+
+        w = rgb.shape[0]
+        h = rgb.shape[1]
         
         return {"processed_number": str(len(cnt))}
     
